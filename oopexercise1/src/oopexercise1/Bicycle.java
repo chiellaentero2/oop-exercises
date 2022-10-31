4bcdbb5f285d09e77d957f8d10b8b71edee6b2a4
Chiella Maryjoy D. Entero <116135700+chiellaentero2@users.noreply.github.com>
2022-10-31 18:26:01 +0800

    oop exercise 1

---
+++ oopexercise1/src/oopexercise1/Bicycle.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopexercise1;
+
+/**
+ *
+ * @author acer
+ */
+public class Bicycle {
+    private String ownerName;
+	private String tagNo;
+	
+	
+	public Bicycle() {
+		ownerName = "Unknown";
+		tagNo = "Unknown";
+	}
+	public Bicycle(String name, String tag){
+		tagNo = tag;
+		ownerName = name;
+	}
+	public String getOwnerName(){
+		return ownerName;
+	}
+	public String getTagNo(){
+		return tagNo;
+	}
+	public void setOwnerName(String name){
+		ownerName = name;
+	}
+	public void setTagNo(String No){
+		tagNo = No;
+		
+	}
+	
+}
